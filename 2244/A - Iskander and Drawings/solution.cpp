79948886ab0351162983f3c8465140664a732b4a
ashish <jipal5212@gmail.com>
2026-09-20 13:14:50 +0530

    Add Iskander and Drawings [A] from Codeforces

---
+++ 2244/A - Iskander and Drawings/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int ans = 0;
+        int cnt = 0;
+        for (char c : s) {
+            if (c == '#') {
+                cnt++;
+            } else {
+                ans = max(ans, (cnt + 1) / 2);
+                cnt = 0;
+            }
+        }
+        ans = max(ans, (cnt + 1) / 2);
+        cout << ans << "
+";
+    }
+    return 0;
+}
