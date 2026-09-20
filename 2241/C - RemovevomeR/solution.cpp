f7355230a8ad21e1d678a573783554c365d23ba8
ashish <jipal5212@gmail.com>
2026-09-20 13:14:28 +0530

    Add RemovevomeR [C] from Codeforces

---
+++ 2241/C - RemovevomeR/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+ 
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] != s[i + 1]) count++;
+        }
+ 
+        cout << (count == 1 ? 2 : 1) << "
+";
+    }
+    return 0;
+}
